37a86f1669183e461c396b00bc3dd6ecc28440c2
Rees Draminski <rees.draminski@gmail.com>
2020-07-03 16:40:15 -0400

    slight fix

--- play/type-racer/app.js
+++ play/type-racer/app.js
@@ -2,9 +2,9 @@ const APP_NAME = "type-racer";
 const DM = new DataManager(APP_NAME);
 
 // the list of words that can show up for the user to type
-const words = shuffle([
+let words = [
     "Adult", "Airplane", "Air", "Aircraft", "Airforce", "Airport", "Album", "Alphabet", "Apple", "Arm", "Army", "Baby", "Baby", "Backpack", "Balloon", "Banana", "Bank", "Barbecue", "Bathroom", "Bathtub", "Bed", "Bee", "Bible", "Bible", "Bird", "Bomb", "Book", "Boss", "Bottle", "Bowl", "Box", "Boy", "Brain", "Bridge", "Butterfly", "Button", "Cappuccino", "Car", "Card", "Carpet", "Carrot", "Cave", "Chair", "Chess", "Chief", "Child", "Chisel", "Chocolates", "Church", "Church", "Circle", "Circus", "Circus", "Clock", "Clown", "Coffee", "Comet", "Compact", "Compass", "Computer", "Crystal", "Cup", "Cycle", "Database", "Desk", "Diamond", "Dress", "Drill", "Drink", "Drum", "Dung", "Ears", "Earth", "Egg", "Electricity", "Elephant", "Eraser", "Explosive", "Eyes", "Family", "Fan", "Feather", "Festival", "Film", "Finger", "Fire", "Floodlight", "Flower", "Foot", "Fork", "Freeway", "Fruit", "Fungus", "Game", "Garden", "Gas", "Gate", "Gemstone", "Girl", "Gloves", "God", "Grapes", "Guitar", "Hammer", "Hat", "Hieroglyph", "Highway", "Horoscope", "Horse", "Hose", "Ice", "Icicle", "Insect", "Jet", "Junk", "Kaleidoscope", "Kitchen", "Knife", "Leather", "Leg", "Library", "Liquid", "Magnet", "Man", "Map", "Maze", "Meat", "Meteor", "Microscope", "Milk", "Milkshake", "Mist", "Money", "Monster", "Mosquito", "Mouth", "Nail", "Navy", "Necklace", "Needle", "Onion", "Paint", "Pants", "Parachute", "Passport", "Pebble", "Pendulum", "Pepper", "Perfume", "Pillow", "Plane", "Planet", "Pocket", "Poster", "Potato", "Printer", "Prison", "Pyramid", "Radar", "Rainbow", "Record", "Restaurant", "Rifle", "Ring", "Robot", "Rock", "Rocket", "Roof", "Room", "Rope", "Saddle", "Salt", "Sandpaper", "Sandwich", "Satellite", "School", "Ship", "Shoes", "Shop", "Shower", "Signature", "Skeleton", "Snail", "Software", "Solid", "Space", "Spectrum", "Sphere", "Spice", "Spiral", "Spoon", "Sports", "Spot", "Square", "Staircase", "Star", "Stomach", "Sun", "Sunglasses", "Surveyor", "Swimming", "Sword", "Table", "Tapestry", "Teeth", "Telescope", "Television", "Tennis", "Thermometer", "Tiger", "Toilet", "Tongue", "Torch", "Torpedo", "Train", "Treadmill", "Triangle", "Tunnel", "Typewriter", "Umbrella", "Vacuum", "Vampire", "Videotape", "Vulture", "Water", "Weapon", "Web", "Wheelchair", "Window", "Woman", "Worm"
-]);
+];
 
 // variables to keep track of user typing statistics
 let wordIndex;
@@ -31,7 +31,6 @@ const ROAD_ANIMATION = "slide 120s linear infinite";
 
     // adjust road sprite
     adjustPlayerAndRoad();
-
 })();
 
 /**
@@ -93,8 +92,8 @@ function stopLightStart() {
 
                 // focus on input box after starting
                 input.focus();
-            }, STOP_LIGHT_TIME)
-        }, STOP_LIGHT_TIME)
+            }, STOP_LIGHT_TIME);
+        }, STOP_LIGHT_TIME);
     }, STOP_LIGHT_TIME);
 }
 
@@ -108,6 +107,8 @@ function startGame() {
     wordIndex = 2;
     typingErrors = 0;
     lettersTyped = 0;
+
+    words = shuffle(words);
 
     // reset the player's car
     document.querySelector("#player").style.left = "0px";
@@ -215,7 +216,6 @@ document.getElementById("input").addEventListener('keyup', () => {
     }
     // If the input is incorrect
     else if (input != currentWord.substring(0, input.length)) {
-
         inputEl.style.color = "red";
         typingErrors++;
     }
@@ -228,6 +228,7 @@ document.getElementById("input").addEventListener('keyup', () => {
  */
 function populateWordTray() {
     let wordTray = document.querySelector("#wordTray");
+    wordTray.innerHTML = "";
 
     for (let i = 0; i < 5; i++) {
         let span = document.createElement("span");
