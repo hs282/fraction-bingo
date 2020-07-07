72c6e8fa9c2a19e8744c48842b087674faba0115
joerob99 <josephnrobinson1999@gmail.com>
2020-07-07 16:54:50 -0400

    Balanced resizing issues in the middle of the game

--- play/type-racer/app.js
+++ play/type-racer/app.js
@@ -19,6 +19,9 @@ let timerID;
 // prevent startGame() from being called more than once before ending
 let gameStarted = false;
 
+// track window width as the game progresses, so players cannot use resizing to their advantage
+let windowWidth;
+
 // offset value needed to center player car and road
 const CENTER_VALUE = "8%";
 
@@ -27,6 +30,9 @@ const STOP_LIGHT_TIME = 1000;
 
 // set the animation value to create a sliding road
 const ROAD_ANIMATION = "slide 120s linear infinite";
+
+// number of words typed that will be required to move the player fully to the other side of the screen
+const WORDS_REQUIRED = 50;
 
 (function initUI() {
     // show instructions modal
@@ -107,6 +113,34 @@ function startGame() {
     // prevent startGame() from being called multiple times before ending
     if (gameStarted) {return;}
     gameStarted = true;
+
+    // set appropriate global window width
+    windowWidth = document.getElementById("wordRace").offsetWidth;
+
+    // upon resizing the window, move the car appropriately left or right to prevent cheating
+    window.onresize = () => {
+
+        let newWindowWidth = document.getElementById("wordRace").offsetWidth;
+        let widthRatio = newWindowWidth / windowWidth;
+        windowWidth = newWindowWidth;
+
+        // calculate how much the player will move
+        let left = document.getElementById("player").style.left;
+
+        // calculate new position of the player
+        let newLeft;
+        if (!left.includes("px")) {
+            newLeft = "0px";
+        }
+        else {
+            left = left.replace("px", "");
+            newLeft = (parseInt(left, 10) * widthRatio) + "px";
+        }
+            
+        // move the player div
+        document.getElementById("player").style.left = newLeft;
+
+    };
 
     // reset stats
     numWords = 0;
@@ -194,6 +228,10 @@ document.getElementById("input").addEventListener('keyup', () => {
         // calculate how much the player will move
         let left = document.getElementById("player").style.left;
         let width = document.getElementById("wordRace").offsetWidth;
+
+        // the width to animate the car every time a word is typed
+        let animateWidth = width / WORDS_REQUIRED;
+        console.log(animateWidth);
             
         // if user is at end of screen. end the game
         if (parseInt(left.replace("px", ""), 10) >= width) {
@@ -203,11 +241,11 @@ document.getElementById("input").addEventListener('keyup', () => {
         // calcualte new position of the player
         let newLeft;
         if (!left.includes("px")) {
-            newLeft = "30px";
+            newLeft = animateWidth + "px";
         }
         else {
             left = left.replace("px", "");
-            newLeft = (parseInt(left, 10) + 30) + "px";
+            newLeft = (parseInt(left, 10) + animateWidth) + "px";
         }
             
         // move the player div
