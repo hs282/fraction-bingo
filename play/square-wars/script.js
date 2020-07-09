004085babf1f87f9129bc5babeb73a425bd63612
Dylandk10 <Dylandk10@gmail.com>
2020-07-09 13:30:46 -0400

    fixed isNumbe rmethod for negative nums

--- play/square-wars/script.js
+++ play/square-wars/script.js
@@ -23,7 +23,7 @@ function setLevel(level) {
 **/
 function isNumber(evt) {
   var iKeyCode = (evt.which) ? evt.which : evt.keyCode
-  if (iKeyCode != 46 && iKeyCode > 31 && (iKeyCode < 48 || iKeyCode > 57)) {
+  if ((iKeyCode != 46 && iKeyCode != 45) && iKeyCode > 31 && (iKeyCode < 48 || iKeyCode > 57)) {
     return false;
   }
   return true;
