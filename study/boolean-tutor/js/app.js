7f758c834a807445798d4832200e7a1ecd300bd4
Rees Draminski <rees.draminski@gmail.com>
2020-07-19 16:15:55 -0400

    Add framework for custom operand number

--- study/boolean-tutor/js/app.js
+++ study/boolean-tutor/js/app.js
@@ -1,27 +1,58 @@
+const exampleEl = document.getElementById("example");
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+
+// the starting probability of boolean expression nesting
+const EASY_DIFF_NESTING_PROBABILITY = 0.80;
+
+// the rate in which the nesting probability diminishes each recursive step
+const PROB_DECAY = 0.50;
+
+// global state variable
 let answer;
 let score = 0;
 
 /**
+ * Initialize UI components when the script starts.
+ */
+(function initUI() {
+    document.querySelector("#answerTrue").onclick  = () => answerPrompt(true);
+    document.querySelector("#answerFalse").onclick = () => answerPrompt(false);
+
+    generateProblem();
+})();
+
+/**
+ * Recursively create an evaluateable boolean expression.
+ * @param {String[]} operands
+ * @param {Number} nestingProbability
  * @returns {String} expression
  */
-function getBooleanExpression(a, b) {
-    let operation = Math.round(Math.random()) ? "&&" : "||";
+function getBooleanExpression(operands, nestingProbability) {
+    // generate boolean expressions from the given operands
+    let expressions = operands.map(x => {
+        // if number is under the probabilty threshold, generate nested expression
+        if (Math.random() < nestingProbability) {
+            return `(${getBooleanExpression(operands, nestingProbability - PROB_DECAY)})`;
+        }
+        else {
+            return x;
+        }
+    });
 
-    let operandOne = Math.random() < 0.7 ? a : `(${getBooleanExpression(a, b)})`;
-    let operandTwo = Math.random() < 0.7 ? b : `(${getBooleanExpression(a, b)})`;
+    // choose the boolean operations that will be performed between expressions
+    let operations = operands.map(_ => {
+        // flip a coin to choose
+        if (Math.round(Math.random())) {
+            return " && ";
+        }
+        else {
+            return " || ";
+        }
+    });
 
-    return `${operandOne} ${operation} ${operandTwo}`;
-}
-
-const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
-function getRandomUniqueIdentifier(prev=[]) {
-    let id = alphabet[Math.floor(Math.random() * alphabet.length)];
-
-    if (prev.length && prev.includes(id)) {
-        return getRandomUniqueIdentifier(prev);
-    }
-
-    return id;
+    // combine the expressions and operations together
+    return expressions.reduce((a, b, i) => [a, b].join(operations[(i - 1) % operations.length]));
 }
 
 /**
@@ -32,32 +63,51 @@ function generateProblem() {
 
     let booleanExpression = start;
 
-    let a = getRandomUniqueIdentifier();
-    let b = getRandomUniqueIdentifier([ a ]);
+    // generate a random number of operands depending on difficulty level
+    // TODO skew towards lower numbers? and change based on diff level
+    let min = 2, max = 2;
+    let numOperands = Math.floor(Math.random() * (max - min + 1)) + min;
+    
+    // generate array of unique operand identifiers
+    let operands =[...Array(numOperands).keys()].reduce((ids, _) => {
+        ids.push(getRandomUniqueIdentifier(ids));
 
+        return ids;
+    }, []);
+
+    // if starts with negation, there needs to be parenthesis around the expression
     if (start == "!") {
-        booleanExpression += `(${getBooleanExpression(a, b)})`;
+        booleanExpression += `(${getBooleanExpression(operands, EASY_DIFF_NESTING_PROBABILITY)})`;
     }
     else {
-        booleanExpression += `${getBooleanExpression(a, b)}`;
+        booleanExpression += `${getBooleanExpression(operands, EASY_DIFF_NESTING_PROBABILITY)}`;
     }
 
-    let aInitialValue = Math.round(Math.random()) ? 1 : 0;
-    let bInitialValue = aInitialValue ? 0 : 1;
+    // generate initial values for the operands
+    let initValues = operands.map(_ => Math.round(Math.random()) ? 1 : 0);
 
-    let expression = `boolean ${a} = ${!!aInitialValue};\nboolean ${b} = ${!!bInitialValue};\n\n${booleanExpression}`;
+    // if all initial values are true, make one false
+    if (initValues.every(x => x === 1)) {
+        initValues[Math.floor(Math.random() * initValues.length)] = 0;
+    }
+    // if all initial values are false, make one true
+    else if (initValues.every(x => !x)) {
+        initValues[Math.floor(Math.random() * initValues.length)] = 1;
+    }
 
-    let executableExpression = booleanExpression.replace(new RegExp(a, "g"), aInitialValue);
-    executableExpression = executableExpression.replace(new RegExp(b, "g"), bInitialValue);
+    // create the boolean expression
+    let expression = initValues.reduce((acc, curr, i) => {
+        return acc + `boolean ${operands[i]} = ${!!curr};\n`;
+    }, "");
+    expression += `\n${booleanExpression}`;
 
+    // create an executable javascript boolean expression to eval()
+    let executableExpression = operands.reduce((acc, curr, i) => {
+        return acc.replace(new RegExp(curr, "g"), initValues[i]);
+    }, booleanExpression);
+
+    // evaluate the expression and make that the answer
     answer = !!eval(executableExpression);
-
-    console.log(answer);
-
-    const exampleEl = document.getElementById("example");
-
-    // clear out anything that could still be in the code prompt element
-    exampleEl.innerHTML = booleanExpression;
 
     // add code mirror className so syntax highlighting works
     exampleEl.className = "cm-s-default";
@@ -108,7 +158,18 @@ function setScore(delta) {
     document.getElementById("score").innerText = "Score: " + score;
 }
 
-document.querySelector("#answerTrue").onclick  = () => answerPrompt(true);
-document.querySelector("#answerFalse").onclick = () => answerPrompt(false);
+/**
+ * Get a random one-letter identifier that does is not a duplicate of
+ * any ones in the prev array.
+ * @param {String[]} prev 
+ * @returns String
+ */
+function getRandomUniqueIdentifier(prev) {
+    let id = alphabet[Math.floor(Math.random() * alphabet.length)];
 
-generateProblem();
+    if (prev.length && prev.includes(id)) {
+        return getRandomUniqueIdentifier(prev);
+    }
+
+    return id;
+}
