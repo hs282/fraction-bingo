fea3b936d63fba3af74fa80e22b4c136809a0b13
Hyunsu Shin <hshin28@jhu.edu>
2020-07-21 00:29:41 -0400

    Fixed spelling error

--- play/fraction-bingo/operations/app.js
+++ play/fraction-bingo/operations/app.js
@@ -286,7 +286,7 @@ function checkDiagonals() {
 function bingo() {
     table.style.display = "none";
     noSolBtn.style.display = "none";
-    document.getElementById("problem").innerHTML = "BINGO!!! Onto the next board :)";
+    document.getElementById("problem").innerHTML = "BINGO!!! On to the next board :)";
     setTimeout(function() {
 	    operands = [];
 	    boardFractions = [];
