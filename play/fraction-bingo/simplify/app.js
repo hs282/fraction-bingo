74feb265dc70255eabcd4ad48982394aaa253169
Hyunsu Shin <hshin28@jhu.edu>
2020-07-31 20:16:10 -0400

    Corrections

--- play/fraction-bingo/simplify/app.js
+++ play/fraction-bingo/simplify/app.js
@@ -1,4 +1,4 @@
-var diffLevel = "",
+let diffLevel = "",
     boardType = "",
     numCells = 0,
     width = 0,
@@ -11,7 +11,32 @@ var diffLevel = "",
     table,
     noSolBtn,
     score = 0,
-    numSolved = 0;
+    numSolved = 0,
+    numWhite = 0,
+    boardMin = 0,
+    boardMax = 0,
+    probMin = 0,
+    probMax = 0;
+
+const EASY_BOARD_MIN = 0;
+const EASY_BOARD_MAX = 4;
+const EASY_PROB_MIN = 0;
+const EASY_PROB_MAX = 16;
+const MED_BOARD_MIN = 5;
+const MED_BOARD_MAX = 8;
+const MED_PROB_MIN = 25;
+const MED_PROB_MAX = 64;
+const HARD_BOARD_MIN = 9;
+const HARD_BOARD_MAX = 12;
+const HARD_PROB_MIN = 81;
+const HARD_PROB_MAX = 144;
+const POINTS = 10;
+const BOARD_THREE_CELLS = 9;
+const BOARD_FOUR_CELLS = 16;
+const BOARD_FIVE_CELLS = 25;
+const BOARD_THREE_WIDTH = 3;
+const BOARD_FOUR_WIDTH = 4;
+const BOARD_FIVE_WIDTH = 5;
 
 function startGame() {
     // Get selected difficulty level
@@ -26,27 +51,24 @@ function startGame() {
     if (document.getElementById("three").checked) {
         document.getElementsByName("board3")[0].style.display = ""; 
 	boardType = "board3";
-	numCells = 9;
-	width = 3;
-	multipliers = [1, 2, 3, 4]
+	numCells = BOARD_THREE_CELLS;
+	width = BOARD_THREE_WIDTH;
     } else if (document.getElementById("four").checked) {
 	document.getElementsByName("board4")[0].style.display = "";
 	boardType = "board4";
-	numCells = 16;
-	width = 4;
-	multipliers = [5, 6, 7, 8];
+	numCells = BOARD_FOUR_CELLS;
+	width = BOARD_FOUR_WIDTH;
     } else if (document.getElementById("five").checked) {
 	document.getElementsByName("board5")[0].style.display = "";
 	boardType = "board5";
-	numCells = 25;
-	width = 5;
-	multipliers = [9, 10, 11, 12];
+	numCells = BOARD_FIVE_CELLS;
+	width = BOARD_FIVE_WIDTH;
     } else {
 	return;
     }
 
-    generateBoardFracts();
-    generateProblemFracts();
+    setMinMax();
+    generateFracts();
     generateProblem();
     fillBoard();
 
@@ -54,52 +76,89 @@ function startGame() {
     document.getElementById("game").style.display = "";
 }
 
-// Generate simplified fractions to display on board cells
-function generateBoardFracts() {
-    let fract;
-    for (var i = 0; i < numCells; i++) {
-	fract = getRandomFraction(0, 15, "simplify");
-	boardFractions.push(fract);
-    } 
+function setMinMax() {
+    if (diffLevel == "easy") {
+        boardMin = EASY_BOARD_MIN;
+        boardMax = EASY_BOARD_MAX;
+	probMin = EASY_PROB_MIN;
+	probMax = EASY_PROB_MAX;
+        multipliers = [1, 2, 3, 4];
+    } else if (diffLevel == "medium") {
+        boardMin = MED_BOARD_MIN;
+        boardMax = MED_BOARD_MAX;
+	probMin = MED_PROB_MIN;
+	probMax = MED_PROB_MAX;
+        multipliers = [5, 6, 7, 8];
+    } else {
+        boardMin = HARD_BOARD_MIN;
+        boardMax = HARD_BOARD_MAX;
+	probMin = HARD_PROB_MIN;
+	probMax = HARD_PROB_MAX;
+        multipliers = [9, 10, 11, 12];
+    }
 }
 
+function generateFracts() {
+    let fract, mult, newFract, numerStr, denomStr, numer, denom;
+    
+    for (let i = 0; i < numCells; i++) {
+	fract = getRandomFraction(boardMin, boardMax, "do not simplify");
+        mult = multipliers[Math.floor(Math.random() * multipliers.length)];
+	numerStr = fract.substr(0, fract.indexOf('/'));
+	denomStr = fract.substr(fract.indexOf('/') + 1);
+	numer = parseInt(numerStr) * mult;
+	denom = parseInt(denomStr) * mult;                                                                                                                     
+	newFract = numer + "/" + denom;
+
+	// Add problem that must be simplified to array of problem fractions
+	problemFractions.push(newFract);
+
+	// Add simplified solution to array of board fractions
+	boardFractions.push(simplifyFract(parseInt(numerStr), parseInt(denomStr)));
+
+	// Extra problems 
+	newFract = getRandomFraction(probMin, probMax, "do not simplify");
+        problemFractions.push(newFract);
+    }
+}
+    
 function getRandomFraction(min, max, simplify) {
     let num;
     let numerator = Math.floor(Math.random() * (max - min + 1)) + min;
+
     if (min == 0) {
 	min = 1;
     }
+
     let denominator = Math.floor(Math.random() * (max - min + 1)) + min;
+
     if (simplify == "simplify") {
 	num = simplifyFract(numerator, denominator);
     } else {
-	//num = `${numerator}&frasl;${denominator}`;
 	num = numerator + "/" + denominator;
     }
+
     return num;
 }
 
 function simplifyFract(numerator, denominator) {
-    //let freq = 0;
     let gcd = function gcd(x, y) {
 	return y ? gcd(y, x%y) : x;
     };
     gcd = gcd(numerator, denominator);
-    //return `${numerator/gcd}&frasl;${denominator/gcd}`;
     return numerator/gcd + "/" + denominator/gcd;
 }
 
 // Fill board table cells with simplified fractions
 function fillBoard() {
     table = document.getElementsByName(boardType)[0];
-    var k = 0;
-    for (var i = 0, row; row = table.rows[i]; i++) {
-	for (var j = 0, col; col = row.cells[j]; j++) {
+    let k = 0;
+
+    for (let i = 0, row; row = table.rows[i]; i++) {
+	for (let j = 0, col; col = row.cells[j]; j++) {
 	    col.innerHTML = boardFractions[k];
 	    col.className = "white";
-	    col.onclick = function () {
-		checkAnswer(this);
-	    };
+	    col.onclick = function(){checkAnswer(this)};
 	    k++;
 	}
     }
@@ -108,53 +167,23 @@ function fillBoard() {
 function checkAnswer(cell) {
     if (cell.className != "green") {
         if (ans == cell.innerHTML) {
-            score += 10;
+            score += POINTS;
 	    numSolved++;
 	    cell.className = "green";
 	    let b = checkBingo();
 	    if (!b) {
-		setTimeout(function() {
-		    generateProblem();
-		    }, 1000);
+		setTimeout(generateProblem, 1000);
 	    }
         }
         else {
 	    if (score != 0) {
-	        score -= 10;
+	        score -= POINTS;
 	    }
-	    $('.list-group').append("<li class='list-group-item'><h5 class='list-group-item-heading'>" + problem + "</h5> <p class='list-group-item-text'><b>Your Answer: </b>" + cell.innerHTML + "<br> <b>Correct: </b>" + ans + "</p> </li>");
+	    document.getElementById("wrongProblem").innerHTML += "<li class='list-group-item'><h5 class='list-group-item-heading'>" + problem + "</h5> <p class='list-group-item-text'><b>Your Answer: </b>" + cell.innerHTML + "<br> <b>Correct: </b>" + ans + "</p> </li>";
 	    cell.className = "red";
-            setTimeout(function() {
-                cell.className = "white";
-            }, 1000);
+            setTimeout(() => cell.className = "white", 1000);
         }
 	document.getElementById("score").innerHTML = "Score: " + score;
-    }
-}
-
-// Generate fractions to display as the prompt
-function generateProblemFracts() {
-    let mult;
-    let newFract;
-    let numerStr;
-    let denomStr;
-    let numer;
-    let denom;
-    boardFractions.forEach(function(fract) {
-	    mult = multipliers[Math.floor(Math.random() * 4)];
-	    numerStr = fract.substr(0, fract.indexOf('/'));
-	    denomStr = fract.substr(fract.indexOf('/') + 1);
-	    numer = parseInt(numerStr) * mult;
-	    denom = parseInt(denomStr) * mult;
-	    //newFract = `${numer}&frasl;${denom}`
-	    newFract = numer + "/" + denom;
-	    
-	    problemFractions.push(newFract);
-	});
-
-    for (var i = 0; i < 2 * numCells; i++) {
-	newFract = getRandomFraction(0, 45, "do not simplify");
-	problemFractions.push(newFract);
     }
 }
 
@@ -172,26 +201,25 @@ function generateProblem() {
 
 function solNotFound() {
     noSolBtn = document.getElementById("noSolution");
-    for (var i = 0, row; row = table.rows[i]; i++) {
-        for (var j = 0, col; col = row.cells[j]; j++) {
+
+    for (let i = 0, row; row = table.rows[i]; i++) {
+        for (let j = 0, col; col = row.cells[j]; j++) {
 	    if (col.className == "white") {
 	        if (ans == col.innerHTML) {
 		    if (score != 0) {
-		        score -= 10;
+		        score -= POINTS;
 		    }
 		    document.getElementById("score").innerHTML = "Score: " + score;
-		    $('.list-group').append("<li class='list-group-item'><h5 class='list-group-item-heading'>" + problem + "</h5> <p class='list-group-item-text'><b>Your Answer: </b> Solution not on board <br> <b>Correct: </b>" + ans + "</p> </li>");
+		    document.getElementById("wrongProblem").innerHTML += "<li class='list-group-item'><h5 class='list-group-item-heading'>" + problem + "</h5> <p class='list-group-item-text'><b>Your Answer: </b> Solution not on board <br> <b>Correct: </b>" + ans + "</p> </li>";
 		    noSolBtn.style.backgroundColor = "red";
-		    setTimeout(function() {
-		        noSolBtn.style.backgroundColor = "";
-		    }, 1000);
+		    setTimeout(() => noSolBtn.style.backgroundColor = "", 1000);
 		    return;
 	        }
 	    }
         }
     }
    
-    score += 10;
+    score += POINTS;
     numSolved++;
     document.getElementById("score").innerHTML = "Score: " + score;
     noSolBtn.style.backgroundColor = "green";
@@ -202,71 +230,68 @@ function solNotFound() {
 }
 
 function checkBingo() {
-    let result;
-    result = checkRows();
-    if (result) {
-       	return true;
-    } 
-    result = checkColumns();
-    if (result) {
-	return true;
-    }
-    result = checkDiagonals();
-    if (result) {
-        return true;
-    }
-    return false;
+    return checkRows() || checkColumns() || checkDiagonals();
 }
 
 function checkRows() {
-    rows: for (var i = 0, row; row = table.rows[i]; i++) {
-        cols: for (var j = 0, col; col = row.cells[j]; j++) {
-	    if (col.className != "green") {
-		continue rows;
-	    }
-	}
-	bingo();
-	return true;
+    for (let i = 0, row; row = table.rows[i]; i++) {
+        for (let j = 0, col; col = row.cells[j]; j++) {
+            if (col.className != "green") {
+                numWhite++;
+            }
+        }
+
+        if (numWhite == 0) {
+            bingo();
+            return true;
+        }
+
+        numWhite = 0;
     }
+
     return false;
 }
 
 function checkColumns() {
-    var row = table.rows[0];
-    cols: for (var i = 0, col; col = row.cells[i]; i++) {
-	rows: for (var j = 0; row = table.rows[j]; j++) {
-	    if (row.cells[i].className != "green") {
-		continue cols;
-	    }
-	}
-	bingo();
-	return true;
+    for (i = 0; i < width; i++) {
+        for (let j = 0; row = table.rows[j]; j++) {
+            if (row.cells[i].className != "green") {
+                numWhite++;
+            }
+        }
+
+        if (numWhite == 0) {
+            bingo();
+            return true;
+        }
+
+        numWhite = 0;
     }
+
     return false;
 }
 
 function checkDiagonals() {
     let diag = true;
-    let cell = 0;
-    for (var i = 0, row; row = table.rows[i]; i++) {
-	if (row.cells[cell].className != "green") {
+
+    for (let i = 0, row; row = table.rows[i]; i++) {
+	if (row.cells[i].className != "green") {
 	    diag = false;
 	    break;
 	}
-	cell++;
     }
+
     if (diag) {
         bingo();
 	return true;
     }
 
-    cell = width - 1;
-    for (var j = 0, row; row = table.rows[j]; j++) {
-	if (row.cells[cell].className != "green") {
+    for (let j = 0, row; row = table.rows[j]; j++) {
+	if (row.cells[width - j - 1].className != "green") {
 	    return false;
 	}
-	cell--;
     }
+
     bingo();
     return true;
 }
@@ -277,8 +302,7 @@ function bingo() {
 	document.getElementById("bingo").style.display = "none";
         boardFractions = [];
 	problemFractions = [];
-	generateBoardFracts();
-	generateProblemFracts();
+	generateFracts();
 	generateProblem();
 	fillBoard();
     }, 2000);
@@ -289,7 +313,9 @@ function endGame() {
     document.getElementById("end").style.display = "";
     document.getElementById("totalScore").innerHTML = score;
     document.getElementById("numSolved").innerHTML = numSolved;
-    if (document.getElementById("wrongProblem").innerHTML != "") {
-	document.getElementById("wrong").style.display = "";	
+    
+    if (document.getElementById("wrongProblem").innerHTML == "") {
+	document.getElementById("wrong").style.display = "none";
+	document.getElementById("allCorrect").style.display = "";
     }
 }
